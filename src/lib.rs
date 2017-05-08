6bc464f5fefa82e9aa0b5a84be48bf9a19e30128
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-05-09 00:41:02 +0100

    commit.

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+extern crate os_type;
+use std::process::Command;
+
+fn get_os() -> String {
+    format!("{:?}", os_type::current_platform())
+}
+
+pub struct SSID {
+    id: String,
+    status: String,
+}
+
+impl SSID {
+    pub fn new_query() {
+        if get_os() == "Windows" {
+            let output = Command::new("netsh")
+            .arg("wlan")
+            .arg("show")
+            .arg("interfaces")
+            .spawn();
+        } else {
+            let output = Command::new("iwconfig")
+            .arg("-r")
+            .spawn();
+        }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_new_query() {
+    let ssid = SSID::new_query();
+    //assert_eq!(true, true); /// !!!
+}
