21443b9e0c06bae9afc3941116448fe224f8e685
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-05-10 12:11:04 +0100

    ....

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ impl SSID {
             .arg(&self.interface)
             .output()
             .expect("failed to execute process");
-            println!("{}", output.stdout);
+            println!("{:?}", output.stdout);
         }
     }
 }
